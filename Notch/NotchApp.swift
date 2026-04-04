fd6951e2efad0b23727f7b77c4e618e968c85d1c
David Ramos <davidramosdelucas@gmail.com>
2026-04-04 12:22:55 +0200

    Initial Commit

---
+++ Notch/NotchApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotchApp.swift
+//  Notch
+//
+//  Created by David Ramos on 04/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct NotchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
